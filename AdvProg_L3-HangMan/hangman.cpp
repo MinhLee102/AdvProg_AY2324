c3c86dce250aad49ee0d1928a05cbbbc1160e7bc
Minh Le <leducminh2212@gmail.com>
2024-03-19 17:06:55 +0700

    Update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -163,7 +163,7 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
-   if (isCharInWord == true)
+   if (isCharInWord(ch, word) == true)
    {
         updateSecretWord(secretWord, ch, word);
         updateEnteredChars(ch, correctChars);
